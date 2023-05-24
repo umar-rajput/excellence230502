1089e2414cb1fe90c0b9c2a6a324f82a46870785
unknown <aman.sahu@fluper.in>
2023-05-24 11:12:27 +0530

    print msg form input

--- first-app/src/components/HelloWorld.vue
+++ first-app/src/components/HelloWorld.vue
@@ -37,7 +37,7 @@ export default {
     }
   },
   mounted(){
-    console.log(this.count);
+    // console.log(this.count);
     this.count=2;
   }
 }
---
+++ first-app/src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="testing">
+        <p>{{ msg }}</p>
+        <p>{{ write }} Testing</p>
+        <input v-model="write" placeholder="Write something">
+    </div>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default {
+    name:`Test`,
+    data:()=> {
+        return{
+            msg:"Hello RAJPUT",
+            write:''
+        }
+    },
+}
+</script>
