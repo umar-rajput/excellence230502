8c23d74f4467d0a33e728d9baacb1c9c098d7bad
unknown <aman.sahu@fluper.in>
2023-05-24 11:36:27 +0530

    input and capital letter button

--- first-app/src/components/Test.vue
+++ first-app/src/components/Test.vue
@@ -3,6 +3,9 @@
         <p>{{ msg }}</p>
         <p>{{ write }} Testing</p>
         <input v-model="write" placeholder="Write something">
+        <button @click="clearInput" class="clearButton">Clear</button> 
+        <br>
+        <button @click="capitalLetter" class="upperCaseButton">Make Input Capital</button>
     </div>
 </template>
 
@@ -17,5 +20,23 @@ export default {
             write:''
         }
     },
+    methods:{
+        clearInput(){
+            this.write='';
+        },
+        capitalLetter(){
+            this.write=this.write.toUpperCase();
+        }
+    }
 }
 </script>
+
+<style>
+    .clearButton{
+        margin-left: 5px;
+    }
+    .upperCaseButton{
+        margin-top: 8px;
+        padding: 0px 54px;
+    }
+</style>
